1ca8b609380b8968244323d6cc111fa6604efe3f
kbanerje <banerjee.krishnendu@gmail.com>
2020-12-03 16:21:25 +0000

    Fixed another set of comments from Cascade

--- contact.php
+++ contact.php
@@ -21,7 +21,6 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->phone = $_POST['comment-phone'];
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
@@ -35,7 +34,6 @@
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['comment-phone'], 'Phone Number');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
